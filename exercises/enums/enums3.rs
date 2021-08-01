0be000bd4a94d98a62e33553e175f0bf20eaa825
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2021-08-01 14:47:44 +0200

    Finished 'Hashmaps'

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Echo(String),
+    Move(Point),
+    ChangeColor((u8, u8, u8))
 }
 
 struct Point {
@@ -37,6 +38,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::Quit => self.quit(),
+            Message::ChangeColor((a, b, c)) => self.change_color((a, b, c)),
+            Message::Echo(a) => self.echo(a),
+            Message::Move(Point {x, y}) => self.position = Point {x, y}
+        }
     }
 }
 
